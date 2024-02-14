7b14e00950c1bda1f26f4eddc27e8e5474836022
krobbi <70552146+krobbi@users.noreply.github.com>
2024-02-14 16:09:54 +0000

    Respond with HTML documents

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+/// The content to respond with for an OK response.
+const OK_CONTENT: &str = include_str!("htdocs/index.html");
+
+/// The content to respond with for a not found response.
+const NOT_FOUND_CONTENT: &str = include_str!("htdocs/404.html");
+
 /// Listen for and handle TCP streams.
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
     }
 }
@@ -17,12 +22,15 @@ fn main() {
 /// Respond to an HTTP request.
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<String> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let (status_line, content) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", OK_CONTENT)
+    } else {
+        ("HTTP/1.1 400 Not Found", NOT_FOUND_CONTENT)
+    };
+
+    let length = content.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{content}");
     stream.write_all(response.as_bytes()).unwrap();
 }
