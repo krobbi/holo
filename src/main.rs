d4460d99cfa5be4b10ece6133fce8ae0b22e8ae7
krobbi <70552146+krobbi@users.noreply.github.com>
2024-02-14 14:27:12 +0000

    Print HTTP request headers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-/// Listen for TCP streams.
+/// Listen for and handle TCP streams.
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+/// Print a TCP stream's HTTP request headers.
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
